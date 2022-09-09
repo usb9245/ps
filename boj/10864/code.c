24e9d5944600bdf31d7a0dc5896b27ab4ae5f2e7
MonBox <34123108+usb9245@users.noreply.github.com>
2022-09-10 07:54:34 +0900

    Create code.c

---
+++ boj/10864/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int f[1001];
+
+int main()
+{
+	int n, m;
+	scanf("%d %d", &n, &m);
+
+	int x;
+	for(int i=0;i<(m*2);i++) {
+		scanf("%d", &x);
+		f[x] += 1;
+	}
+
+	for(int i=1;i<=n;i++)
+		printf("%d\n", f[i]);
+
+	return 0;
+}
