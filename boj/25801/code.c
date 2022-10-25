6915c7d0282550f2e6408f1cd02ec388b7101f63
MonBox <34123108+usb9245@users.noreply.github.com>
2022-10-25 19:03:44 +0900

    Create code.c

---
+++ boj/25801/code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int cnt[26];
+
+int main()
+{
+    char c;
+
+    while(~(c = getchar())) 
+        if('a' <= c && c <= 'z') cnt[c - 'a'] += 1;
+    
+    int f;
+    for(int i=0;i<26;i++)
+        if(cnt[i] > 0) {
+            f = i;
+            break;
+        }
+
+    for(int i=1;i<26;i++) 
+        if((cnt[i] > 0) && ((cnt[f] % 2) != (cnt[i] % 2))) {
+            puts("0/1");
+            return 0;
+        }
+    
+    printf("%d", cnt[f] % 2);
+
+    return 0;
+}
