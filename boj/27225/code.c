9ec1b96143f9119891bff5842dfcad0e8e2c3867
MonBox <34123108+usb9245@users.noreply.github.com>
2023-01-27 22:39:10 +0900

    Update code.c

--- boj/27225/code.c
+++ boj/27225/code.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-inline int min(int a, int b) { return a < b ? a : b; }
+inline int min(int a, int b) { return a < b ?  a : b; }
+inline int abs(int n)        { return n < 0 ? -n : n; }
 
 int main()
 {
     int m, n;
     scanf("%d %d", &n, &m);
     
-    printf("%d", (min(n, m) * 2) + (((n + m) - (min(n, m) * 2)) % 2));
+    printf("%d", (min(n, m) * 2) + (abs(n - m) % 2));
     
     return 0;
 }
